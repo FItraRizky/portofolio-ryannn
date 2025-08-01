4f858feddae615978ec60a78294a6c9a310a9c3a
fitrarizky <orian3969@gmail.com>
2025-08-01 13:36:04 +0700

    vsd

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -5,7 +5,6 @@ import { skills, experiences, education } from '../../utils/data';
 import { fadeInUp, fadeInLeft, fadeInRight } from '../../utils/animations';
 import { Card, CardHeader, CardBody } from '../ui/Card';
 import Hyperspeed from '../../backgrounds/Hyperspeed/Hyperspeed';
-import { hyperspeedPresets } from '../../backgrounds/Hyperspeed/HyperSpeedPresets';
 
 import './AboutSection.css';
 
@@ -90,7 +89,41 @@ const AboutSection: React.FC = () => {
     <section id="about" className="about" ref={ref}>
       {/* Hyperspeed Background */}
       <div className="about__background">
-        <Hyperspeed effectOptions={hyperspeedPresets.one} />
+        <Hyperspeed effectOptions={{
+          distortion: "turbulentDistortion",
+          length: 400,
+          roadWidth: 10,
+          islandWidth: 2,
+          lanesPerRoad: 3,
+          fov: 90,
+          fovSpeedUp: 150,
+          speedUp: 2,
+          carLightsFade: 0.4,
+          totalSideLightSticks: 20,
+          lightPairsPerRoadWay: 40,
+          shoulderLinesWidthPercentage: 0.05,
+          brokenLinesWidthPercentage: 0.1,
+          brokenLinesLengthPercentage: 0.5,
+          lightStickWidth: [0.12, 0.5] as [number, number],
+          lightStickHeight: [1.3, 1.7] as [number, number],
+          movingAwaySpeed: [60, 80] as [number, number],
+          movingCloserSpeed: [-120, -160] as [number, number],
+          carLightsLength: [12, 80] as [number, number],
+          carLightsRadius: [0.05, 0.14] as [number, number],
+          carWidthPercentage: [0.3, 0.5] as [number, number],
+          carShiftX: [-0.8, 0.8] as [number, number],
+          carFloorSeparation: [0, 5] as [number, number],
+          colors: {
+            roadColor: 0x080808,
+            islandColor: 0x0a0a0a,
+            background: 0x000000,
+            shoulderLines: 0x131318,
+            brokenLines: 0x131318,
+            leftCars: [0xd856bf, 0x6750a2, 0xc247ac],
+            rightCars: [0x03b3c3, 0x0e5ea5, 0x324555],
+            sticks: 0x03b3c3,
+          }
+        }} />
       </div>
       <div className="about__container">
         {/* Section Header */}
